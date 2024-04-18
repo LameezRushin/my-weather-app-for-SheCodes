46f83ec4c24b047204248e9962f371cfab415c32
LameezRushin <162303399+LameezRushin@users.noreply.github.com>
2024-04-18 11:56:46 +0200

    Added the apiKey  JS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
+function searchCity(city) {
+  let apiKey = "8019d34f095be575353b45c55a660t0o";
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
