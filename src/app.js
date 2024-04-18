7d3f8eff36752e680fdf522300849c8db2761862
LameezRushin <162303399+LameezRushin@users.noreply.github.com>
2024-04-18 15:48:58 +0200

    apiKey, apiUrl & button CSS added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 function searchCity(city) {
   let apiKey = "8019d34f095be575353b45c55a660t0o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 }
 
 function searchSubmit(event) {
